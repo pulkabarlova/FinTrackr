b9785bec6d1687c8089dfa43449e14fae64b5bd7
polina <barlovapolina@gmail.com>
2025-06-09 23:22:46 +0200

    feature(incomes): ui for incomes added

--- app/src/main/java/com/polina/fintrackr/features/incoms/IncomesScreen.kt
+++ app/src/main/java/com/polina/fintrackr/features/incoms/IncomesScreen.kt
@@ -1,22 +1,46 @@
 package com.polina.fintrackr.features.incoms
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.polina.fintrackr.R
+import com.polina.fintrackr.core.theme.FinTrackrTheme
 import com.polina.fintrackr.core.ui.AppScaffold
+
 
 @Composable
 fun IncomesScreen(navController: NavController) {
     Text("Incomes")
     AppScaffold(
         navController = navController,
-        content = {Text("Incomes") },
+        content = { paddingValues -> Content(paddingValues = paddingValues, value = "10000$") },
         topBar = { TopBar() })
 }
 
@@ -26,11 +50,136 @@ fun TopBar() {
     CenterAlignedTopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primary,
-            titleContentColor = MaterialTheme.colorScheme.onPrimary,),
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+        ),
         title = {
-            Text(
-                text = stringResource(id = R.string.incomes),
-                color = MaterialTheme.colorScheme.onPrimary
-            ) }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.my_incomes),
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+
+                Icon(
+                    painter = painterResource(R.drawable.trailing_icon),
+                    contentDescription = "trailing_icon",
+                    tint = MaterialTheme.colorScheme.onPrimary,
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                )
+            }
+        }
+
     )
+}
+
+@Composable
+fun Content(paddingValues: androidx.compose.foundation.layout.PaddingValues, value: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            item { IncomesSingleItem(value) }
+            repeat(2) {
+                item { IncomesNavItem("Текст") }
+            }
+        }
+
+        FloatingActionButton(
+            onClick = { /* TODO: Handle click */ },
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+            shape = CircleShape,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "",
+                tint = Color.White
+            )
+        }
+    }
+}
+
+@Composable
+fun IncomesNavItem(title: String, onClick: () -> Unit = {}) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .border(
+                width = 0.5.dp,
+                color = Color.Gray,
+            )
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(vertical = 12.dp),
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            text = title,
+            modifier = Modifier.padding(vertical = 12.dp),
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Icon(
+            painter = painterResource(R.drawable.arrow_right),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(vertical = 7.dp)
+                .padding(10.dp)
+        )
+    }
+}
+
+@Composable
+fun IncomesSingleItem(value: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                width = 0.5.dp,
+                color = Color.Gray,
+            )
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(R.string.all),
+            modifier = Modifier.padding(vertical = 6.dp),
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            text = value,
+            modifier = Modifier.padding(vertical = 6.dp),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Preview(name = "Light Mode", showSystemUi = true)
+@Composable
+fun SettingsLoggedScreenPreview() {
+    FinTrackrTheme {
+        IncomesScreen(navController = NavController(LocalContext.current))
+    }
 }
